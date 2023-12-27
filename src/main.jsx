b354cd1c20624bd32f2d4a34094e7358357df7cd
Tuns0704 <tuanhm0704@gmail.com>
2023-12-28 01:02:45 +0700

    _redirects

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,14 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { ThemeProvider } from "@material-tailwind/react";
 import AuthContext from "./cores/context/app.context.jsx";
+import { AppRouter } from "./routes/app.router.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
 		<BrowserRouter>
 			<ThemeProvider>
 				<AuthContext>
+					<AppRouter />
 					<App />
 				</AuthContext>
 			</ThemeProvider>
